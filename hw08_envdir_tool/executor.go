e09c19ed4e2d9ffbdce0ff700ce041077aa421d8
Evgeny2015 <105362989+Evgeny2015@users.noreply.github.com>
2026-02-15 17:43:06 +0300

    realize RunCmd with tests

--- hw08_envdir_tool/executor.go
+++ hw08_envdir_tool/executor.go
@@ -1,7 +1,60 @@
 package main
 
+import (
+	"fmt"
+	"os"
+	"os/exec"
+	"strings"
+)
+
 // RunCmd runs a command + arguments (cmd) with environment variables from env.
 func RunCmd(cmd []string, env Environment) (returnCode int) {
-	// Place your code here.
-	return
+
+	command := exec.Command(cmd[0], cmd[1:]...)
+
+	// get system environment variables
+	envMap := KeyValueArrayToMap(os.Environ())
+
+	// append/delete environment variables to command
+	for k, v := range env {
+		if v.NeedRemove {
+			delete(envMap, k)
+		} else {
+			envMap[k] = v.Value
+		}
+	}
+
+	// convert map to array
+	cmdEnv := []string{}
+	for k, v := range envMap {
+		cmdEnv = append(cmdEnv, fmt.Sprintf("%s=%s", k, v))
+	}
+	command.Env = cmdEnv
+
+	// set stdin, stdout, stderr
+	command.Stdout = os.Stdout
+	command.Stderr = os.Stderr
+	command.Stdin = os.Stdin
+
+	// run command
+	err := command.Run()
+	if err != nil {
+		if exitError, ok := err.(*exec.ExitError); ok {
+			return exitError.ExitCode()
+		} else {
+			return 127
+		}
+	}
+
+	return 0
+}
+
+// convert key=value string array to map
+func KeyValueArrayToMap(kv []string) map[string]string {
+	env := make(map[string]string)
+	for _, kv := range kv {
+		s := strings.Split(kv, "=")
+		env[s[0]] = s[1]
+	}
+	return env
 }
--- hw08_envdir_tool/executor_test.go
+++ hw08_envdir_tool/executor_test.go
@@ -3,5 +3,19 @@ package main
 import "testing"
 
 func TestRunCmd(t *testing.T) {
-	// Place your code here
+	env := Environment{
+		"BAR":   EnvValue{Value: "bar", NeedRemove: false},
+		"EMPTY": EnvValue{Value: "", NeedRemove: false},
+		"FOO":   EnvValue{Value: "   foo\nwith new line", NeedRemove: false},
+		"HELLO": EnvValue{Value: "\"hello\"", NeedRemove: false},
+		"UNSET": EnvValue{Value: "", NeedRemove: true},
+	}
+
+	t.Run("simple command without params", func(t *testing.T) {
+		RunCmd([]string{"ping"}, env)
+	})
+
+	t.Run("simple command", func(t *testing.T) {
+		RunCmd([]string{"ping", "127.0.0.1"}, env)
+	})
 }
