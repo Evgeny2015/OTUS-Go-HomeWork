3c20ee43713054611ae890d36d97fb22b3a58f20
Evgeny2015 <105362989+Evgeny2015@users.noreply.github.com>
2026-02-18 23:37:18 +0300

    HW09 is completed

--- hw09_struct_validator/validator.go
+++ hw09_struct_validator/validator.go
@@ -1,5 +1,22 @@
 package hw09structvalidator
 
+import (
+	"errors"
+	"reflect"
+	"regexp"
+	"slices"
+	"strconv"
+	"strings"
+)
+
+var (
+	ErrInvalidStringLength = errors.New("invalid string length")
+	ErrValueMin            = errors.New("value too small")
+	ErrValueMax            = errors.New("value too large")
+	ErrInvalidItem         = errors.New("invalid item")
+	ErrInvalidReqExp       = errors.New("invalid regexp")
+)
+
 type ValidationError struct {
 	Field string
 	Err   error
@@ -8,10 +25,152 @@ type ValidationError struct {
 type ValidationErrors []ValidationError
 
 func (v ValidationErrors) Error() string {
-	panic("implement me")
+	result := ""
+	for i, err := range v {
+		if i > 0 {
+			result += "; "
+		}
+		result += err.Field + ": " + err.Err.Error()
+	}
+	return result
 }
 
 func Validate(v interface{}) error {
-	// Place your code here.
+	errors := ValidationErrors{}
+
+	// if v is not a struct, return nil
+	rv := reflect.ValueOf(v)
+	if rv.Kind() != reflect.Struct {
+		return nil
+	}
+
+	// if v is a struct, validate each field
+	if rv.Kind() == reflect.Struct {
+		types := rv.Type()
+
+		for i := 0; i < rv.NumField(); i++ {
+			field := types.Field(i)
+
+			// validate field
+			err := ValidateField(field, rv.Field(i))
+			if err != nil {
+				errors = append(errors, ValidationError{Field: field.Name, Err: err})
+			}
+		}
+	}
+
+	// if no errors, return nil
+	if len(errors) == 0 {
+		return nil
+	}
+	return errors
+}
+
+// validate field.
+func ValidateField(field reflect.StructField, value reflect.Value) error {
+	validation := field.Tag.Get("validate")
+	if validation == "" {
+		return nil
+	}
+
+	switch value.Type().Kind() {
+	case reflect.String:
+		return validateString(value.String(), validation)
+	case reflect.Int:
+		return validateInt(int(value.Int()), validation)
+	case reflect.Slice:
+		for i := range value.Seq() {
+			err := ValidateField(field, value.Index(int(i.Int())))
+			if err != nil {
+				return err
+			}
+		}
+	case reflect.Invalid, reflect.Bool, reflect.Int8, reflect.Int16, reflect.Int32, reflect.Int64,
+		reflect.Uint, reflect.Uint8, reflect.Uint16, reflect.Uint32, reflect.Uint64, reflect.Uintptr,
+		reflect.Float32, reflect.Float64, reflect.Complex64, reflect.Complex128, reflect.Array,
+		reflect.Chan, reflect.Func, reflect.Interface, reflect.Map, reflect.Ptr, reflect.Struct,
+		reflect.UnsafePointer:
+		return nil
+	default:
+		return nil
+	}
+
+	return nil
+}
+
+// get validation rules.
+func getValidationRules(validation string) map[string]interface{} {
+	rules := map[string]interface{}{}
+
+	// split validation into rules
+	for _, rule := range strings.Split(validation, "|") {
+		kv := strings.Split(rule, ":")
+		switch kv[0] {
+		case "len", "min", "max":
+			v, err := strconv.Atoi(kv[1])
+			if err == nil {
+				rules[kv[0]] = v
+			}
+		case "in":
+			rules[kv[0]] = strings.Split(kv[1], ",")
+		case "regexp":
+			rules[kv[0]] = kv[1]
+		}
+	}
+
+	return rules
+}
+
+// validateString validates a string against a set of rules.
+func validateString(value string, validation string) error {
+	rules := getValidationRules(validation)
+
+	for rule, constraint := range rules {
+		switch rule {
+		// validate string length
+		case "len":
+			if len(value) != constraint.(int) {
+				return ErrInvalidStringLength
+			}
+		// validate string in list
+		case "in":
+			if !slices.Contains(constraint.([]string), value) {
+				return ErrInvalidItem
+			}
+		// validate regexp
+		case "regexp":
+			matched, err := regexp.MatchString(constraint.(string), value)
+			if err != nil {
+				return err
+			}
+			if !matched {
+				return ErrInvalidReqExp
+			}
+		}
+	}
+	return nil
+}
+
+// validateInt validates an int against a set of rules.
+func validateInt(value int, validation string) error {
+	rules := getValidationRules(validation)
+
+	for rule, constraint := range rules {
+		switch rule {
+		case "min":
+			if value < constraint.(int) {
+				return ErrValueMin
+			}
+		case "max":
+			if value > constraint.(int) {
+				return ErrValueMax
+			}
+		case "in":
+			if !slices.Contains(constraint.([]string), strconv.Itoa(value)) {
+				return ErrInvalidItem
+			}
+		}
+	}
+
 	return nil
 }
--- hw09_struct_validator/validator_test.go
+++ hw09_struct_validator/validator_test.go
@@ -2,8 +2,11 @@ package hw09structvalidator
 
 import (
 	"encoding/json"
+	"errors"
 	"fmt"
 	"testing"
+
+	"github.com/stretchr/testify/require"
 )
 
 type UserRole string
@@ -42,10 +45,61 @@ func TestValidate(t *testing.T) {
 		expectedErr error
 	}{
 		{
-			// Place your code here.
+			in: User{
+				ID:     "123456789012345678901234567890123456",
+				Name:   "John Doe",
+				Age:    30,
+				Email:  "johndoe@example.com",
+				Role:   UserRole("admin"),
+				Phones: []string{"12345678901"},
+			},
+			expectedErr: nil,
 		},
-		// ...
-		// Place your code here.
+		{
+			in: User{
+				ID:     "12345678901234567890123456789012345",
+				Name:   "John Doe",
+				Age:    30,
+				Email:  "johndoe@example.com",
+				Role:   UserRole("admin"),
+				Phones: []string{"12345678901"},
+			},
+			expectedErr: ErrInvalidStringLength,
+		},
+		{
+			in: App{
+				Version: "1.0.0",
+			},
+			expectedErr: nil,
+		},
+		{
+			in: App{
+				Version: "1.0.0.1",
+			},
+			expectedErr: ErrInvalidStringLength,
+		},
+		{
+			in: Token{
+				Header:    []byte("header"),
+				Payload:   []byte("payload"),
+				Signature: []byte("signature"),
+			},
+			expectedErr: nil,
+		},
+		{
+			in: Response{
+				Code: 200,
+				Body: "OK",
+			},
+			expectedErr: nil,
+		},
+		{
+			in: Response{
+				Code: 201,
+				Body: "OK",
+			},
+			expectedErr: ErrInvalidItem,
+		},
 	}
 
 	for i, tt := range tests {
@@ -54,7 +108,120 @@ func TestValidate(t *testing.T) {
 			t.Parallel()
 
 			// Place your code here.
+			err := Validate(tt.in)
+
+			// If no error is expected
+			if tt.expectedErr == nil {
+				if err != nil {
+					t.Errorf("Validate() error = %v, expectedErr %v", err, tt.expectedErr)
+				}
+				return
+			}
+
+			// If an error is expected but none occurred
+			if err == nil {
+				t.Errorf("Validate() error = %v, expectedErr %v", err, tt.expectedErr)
+				return
+			}
+
+			// Check if any of the validation errors match the expected error
+			found := false
+			for _, vErr := range err.(ValidationErrors) { //nolint:errorlint
+				if errors.Is(vErr.Err, tt.expectedErr) {
+					found = true
+					break
+				}
+			}
+
+			if !found {
+				t.Errorf("Validate() error = %v, expectedErr %v", err, tt.expectedErr)
+			}
 			_ = tt
 		})
 	}
+}
+
+func TestUser(t *testing.T) {
+	user := User{
+		ID:     "123456789012345678901234567890123456",
+		Name:   "John Doe",
+		Age:    30,
+		Email:  "johndoe@example.com",
+		Role:   UserRole("admin"),
+		Phones: []string{"12345678901"},
+	}
+
+	t.Run("all valid", func(t *testing.T) {
+		err := Validate(user)
+		require.NoError(t, err)
+	})
+
+	t.Run("user id", func(t *testing.T) {
+		user.ID = "12345678901234567890123456789"
+		err := Validate(user)
+		require.ErrorContains(t, err, ErrInvalidStringLength.Error())
+	})
+
+	t.Run("user email", func(t *testing.T) {
+		user.Email = "john.doe@example.com"
+		err := Validate(user)
+		require.ErrorContains(t, err, ErrInvalidReqExp.Error())
+	})
+
+	t.Run("user role", func(t *testing.T) {
+		user.Role = "no"
+		err := Validate(user)
+		require.ErrorContains(t, err, ErrInvalidItem.Error())
+	})
+
+	t.Run("user phones", func(t *testing.T) {
+		user.Phones = []string{"12345678901", "1234567890"}
+		err := Validate(user)
+		fmt.Println(err)
+		require.ErrorContains(t, err, ErrInvalidStringLength.Error())
+	})
+
+	t.Run("user age", func(t *testing.T) {
+		user.Age = 10
+		err := Validate(user)
+		require.ErrorContains(t, err, ErrValueMin.Error())
+
+		user.Age = 60
+		err = Validate(user)
+		require.ErrorContains(t, err, ErrValueMax.Error())
+	})
+}
+
+func TestApp(t *testing.T) {
+	app := App{
+		Version: "1.0.0",
+	}
+	t.Run("all valid", func(t *testing.T) {
+		err := Validate(app)
+		require.NoError(t, err)
+	})
+
+	t.Run("app version", func(t *testing.T) {
+		app.Version = "1.0.1.0"
+		err := Validate(app)
+		require.ErrorContains(t, err, ErrInvalidStringLength.Error())
+	})
+}
+
+func TestResponse(t *testing.T) {
+	response := Response{
+		Code: 200,
+		Body: "OK",
+	}
+
+	t.Run("all valid", func(t *testing.T) {
+		err := Validate(response)
+		require.NoError(t, err)
+	})
+
+	t.Run("response code", func(t *testing.T) {
+		response.Code = 201
+		err := Validate(response)
+		require.ErrorContains(t, err, ErrInvalidItem.Error())
+	})
 }
